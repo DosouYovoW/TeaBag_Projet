4055f1df24259b1b2f51ec6b154e516936b15cae
DosouYovoW <wilfrieddossouyovo16@gmail.com>
2020-02-27 21:54:19 -0500

    Ajout de commentaire2

--- brouillon/SUJET SPECIAL.R
+++
@@ -1,7 +0,0 @@
-library(psych)
-var(bfi[,1:4], na.rm=T)->ex.var
-
-donnee <- read.csv("TeaBag3_.csv",
-                     header = TRUE, stringsAsFactors = TRUE,
-                     dec=',', sep=';',  na.strings = "")
-str(donnee)
